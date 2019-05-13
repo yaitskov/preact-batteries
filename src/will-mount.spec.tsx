e8479aca2992fbc36b75b09df916e8323aef1b4d
Daneel Yaitskov <dyaitskov@gmail.com>
2019-05-13 16:22:47 -0700

    test will-mount test dom

---
+++ src/will-mount.spec.tsx
@@ -0,0 +1,13 @@
+import {h} from 'preact';
+import {shallow} from 'preact-render-spy';
+import { Wm } from './will-mount';
+
+
+describe('Will mount component', () => {
+  it('callback is called', () => {
+    const calls = [];
+    const dom = shallow(<Wm c={() => calls.push(1)} />);
+    expect(dom.find('b').length).toBe(1);
+    expect(calls).toEqual([1]);
+  });
+});
