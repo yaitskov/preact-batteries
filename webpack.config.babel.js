50a54d52a0a0c932c4364e063768670a1713f99a
Daneel Yaitskov <dyaitskov@gmail.com>
2019-05-12 17:34:28 -0700

    test extract and unify webpack module config into a file

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -12,33 +12,7 @@ module.exports = {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
   },
-  module: {
-    rules: [
-      // Handle .ts and .tsx file via ts-loader.
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [['@babel/typescript', { jsxPragma: "h" }]],
-            plugins: [
-              ["@babel/plugin-proposal-function-bind"],
-              ['@babel/proposal-class-properties',
-               {
-                 'loose': false
-               }
-              ],
-              ['@babel/plugin-proposal-throw-expressions'],
-              ['@babel/proposal-object-rest-spread'],
-              ['@babel/plugin-syntax-dynamic-import'],
-              ["@babel/transform-react-jsx", { "pragma": "h" }]
-            ]
-          }
-        }
-      }
-    ]
-  },
+  module: require('./webpack.modules.js'),
   plugins: [
     new HtmlWebpackPlugin()
   ]
