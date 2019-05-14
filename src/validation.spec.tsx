d6def0f5a974b45791dc9a5645b3051b33bd7e8f
Daneel Yaitskov <dyaitskov@gmail.com>
2019-05-13 21:07:45 -0700

    test int validator test

--- src/validation.spec.tsx
+++ src/validation.spec.tsx
@@ -1,11 +1,25 @@
 import { asyncIt, isA } from './test-utils';
 import { Invalid } from './invalid';
-import { Max, IntType, NotEmpty } from './validation';
+import { Validator, Max, IntType, NotEmpty } from './validation';
+
+function pass(msg: string, validator: Validator, value: string) {
+  asyncIt('pass ' + msg, validator.check(value), c => c.toEqual([]));
+}
+
+function reject(msg: string, validator: Validator, value: string) {
+  asyncIt('reject ' + msg, validator.check(value), c => c.toEqual([isA(Invalid)]));
+}
 
 describe('validation', () => {
   describe('max', () => {
     asyncIt('pass eq', new Max(10).check('10'), c => c.toEqual([]));
     asyncIt('pass less', new Max(10).check('9'), c => c.toEqual([]));
     asyncIt('reject more', new Max(10).check('11'), c => c.toEqual([isA(Invalid)]));
+  });
+
+  describe('int type', () => {
+    pass('zero', new IntType(), '0');
+    reject('negative', new IntType(), '-9');
+    reject('letter', new IntType(), '1l1');
   });
 });
