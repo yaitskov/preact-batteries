9b18544b1fc82c78c4f28b46d5ea25dd62a9dea6
Daneel Yaitskov <dyaitskov@gmail.com>
2019-05-16 16:19:09 -0700

    test fix types for if.spec

--- src/component/if.spec.tsx
+++ src/component/if.spec.tsx
@@ -4,9 +4,9 @@ import { If, IfSt } from './if';
 
 describe('If component', () => {
   it('display is block when visible', () =>
-    expect(shallow(<If f={true} />).find('div').at(0).attr('style'))
-      .toEqual({display: "block"}));
+    expect(shallow(<If f={true} />).find('div').at(0).attrs())
+      .toEqual({style: {display: "block"}}));
   it('display is none when invisible', () =>
-    expect(shallow(<If f={false} />).find('div').at(0).attr('style'))
-      .toEqual({display: "none"}));
+    expect(shallow(<If f={false} />).find('div').at(0).attrs())
+      .toEqual({style: {display: "none"}}));
 });
