4e1ce9f1456cc4ed61c324f0b41dce3c5ba00ea5
Daneel Yaitskov <dyaitskov@gmail.com>
2019-05-30 10:48:06 -0700

    brotli compress bundles with brolti and gzip

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,9 @@
 const fPath = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MergeAndFlushI18nPlugin = require('./merge-and-flush-i18n');
+const BrotliPlugin = require('brotli-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
 const makeWebPackModule = require('./webpack.modules.js');
 const translationPlugin = require('./translation-plugin.js');
 
@@ -27,6 +30,19 @@ module.exports = {
   module: makeWebPackModule([ visitorFactory ]),
   plugins: [
     new HtmlWebpackPlugin(),
-    new MergeAndFlushI18nPlugin(state)
+    new MergeAndFlushI18nPlugin(state),
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 1024,
+      minRatio: 0.8
+    }),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 1024,
+      minRatio: 0.8
+    })
   ]
 };
