2c02b7365bd2b57b3efa58194d81030b608a8df5
Daneel Yaitskov <dyaitskov@gmail.com>
2019-06-07 08:22:52 -0700

    modular-css add menu on landing page

--- src/app/page/landing-page.tsx
+++ src/app/page/landing-page.tsx
@@ -1,62 +1,88 @@
 import { h } from 'preact';
-import { InjSubCom } from 'injection/inject-sub-components';
+import { TransCom, TransComS } from 'i18n/trans-component';
+import { TitleMainMenu } from 'app/component/title-main-menu';
+import { NavbarLinkItem } from 'app/component/navbar-link-item';
+import { T } from 'i18n/translate-tag';
 
 import bulma from 'bulma/css/bulma.css';
 
-export default class LandingPage extends InjSubCom<{}, {}> {
-  render() {
-    return <section class={bulma.section}>
-      <div class={bulma.container}>
-        <div class={bulma.tile}>
-          <article class={bulma.tile}>
-            <p class={bulma.title}>
-              No paper work!
-            </p>
-            <p class={bulma.subtitle}>
-            </p>
-          </article>
-        </div>
+export interface LandingPageS extends TransComS {
+}
 
-        <div class={bulma.tile}>
-          <article class={bulma.tile}>
-            <p class={bulma.title}>
-              No long lines and jams!
-            </p>
-            <p class={bulma.subtitle}>
-            </p>
-          </article>
-        </div>
-
-        <div class={bulma.tile}>
-          <article class={bulma.tile}>
-            <p class={bulma.title}>
-              Referee enjoy watching not a book keeping!
-            </p>
-            <p class={bulma.subtitle}>
-            </p>
-          </article>
-        </div>
-
-        <div class={bulma.tile}>
-          <article class={bulma.tile}>
-            <p class={bulma.title}>
-              Share tournament outcome on Facebook in one click!
-            </p>
-            <p class={bulma.subtitle}>
-            </p>
-          </article>
-        </div>
-
-        <div class={bulma.tile}>
-          <article class={bulma.tile}>
-            <p class={bulma.title}>
-              Don't worry about your turns and relax in a cafe!
-            </p>
-            <p class={bulma.subtitle}>
-            </p>
-          </article>
-        </div>
-      </div>
-    </section>;
+export default class LandingPage extends TransCom<{}, LandingPageS> {
+  constructor(props) {
+    super(props);
+    this.st = {at: this.at()};
   }
+
+  render() {
+    const [TI, TitleMainMenuI] = this.c2(T, TitleMainMenu);
+    return <div>
+      <TitleMainMenuI
+        t$title="Welcome to Cloud-Sport"
+        menuItems={
+          [
+            <NavbarLinkItem path="/draft" t$label="Draft to coming tournaments" />,
+            <NavbarLinkItem path="/watch" t$label="Watch ongoing tournaments" />,
+            <hr class={bulma.navbarDivider}/>,
+            <NavbarLinkItem path="/terms" t$label="Terms of service" />
+          ]
+        } />
+      <section class={bulma.section}>
+        <div class={bulma.container}>
+          <div class={bulma.tile}>
+            <article class={bulma.tile}>
+              <p class={bulma.title}>
+                <TI m="No paper work!" />
+              </p>
+              <p class={bulma.subtitle}>
+              </p>
+            </article>
+          </div>
+
+          <div class={bulma.tile}>
+            <article class={bulma.tile}>
+              <p class={bulma.title}>
+                <TI m="No long lines and jams!"/>
+              </p>
+              <p class={bulma.subtitle}>
+              </p>
+            </article>
+          </div>
+
+          <div class={bulma.tile}>
+            <article class={bulma.tile}>
+              <p class={bulma.title}>
+                <TI m="Referee enjoy watching not a book keeping!"/>
+              </p>
+              <p class={bulma.subtitle}>
+              </p>
+            </article>
+          </div>
+
+          <div class={bulma.tile}>
+            <article class={bulma.tile}>
+              <p class={bulma.title}>
+                <TI m="Share tournament outcome on Facebook in one click!" />
+              </p>
+              <p class={bulma.subtitle}>
+              </p>
+            </article>
+          </div>
+
+          <div class={bulma.tile}>
+            <article class={bulma.tile}>
+              <p class={bulma.title}>
+                <TI m="Don't worry about your turns and relax in a cafe!" />
+              </p>
+              <p class={bulma.subtitle}>
+              </p>
+            </article>
+          </div>
+        </div>
+      </section>
+    </div>;
+  }
+
+  at(): string[] { return []; }
 }
