d977371042d10c90b801028454e640b0fe29f260
Daneel Yaitskov <dyaitskov@gmail.com>
2019-06-09 09:33:28 -0700

    modular-css toggle main menu

--- src/app/component/title-main-menu.tsx
+++ src/app/component/title-main-menu.tsx
@@ -1,12 +1,13 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import { Link } from 'preact-router';
 import { InjSubCom } from 'injection/inject-sub-components';
 import { T } from 'i18n/translate-tag';
-import bulma from 'bulma/bulma.sass';
 import { SuperElement } from 'component/types';
 import { UserAuth } from 'app/auth/user-auth';
 import { If } from 'component/if';
 import { LogoutBtn } from 'app/component/logout-button';
+
+import bulma from 'bulma/bulma.sass';
 
 export interface TitleMainMenuP {
   t$title: string;
@@ -14,21 +15,33 @@ export interface TitleMainMenuP {
 }
 
 interface TitleMainMenuS {
+  showMenu: boolean;
 }
 
 export class TitleMainMenu extends InjSubCom<TitleMainMenuP, TitleMainMenuS> {
   // @ts-ignore
   private $userAuth: UserAuth;
 
+  toggleMenu() {
+    this.ust(st => ({...st, showMenu: !st.showMenu}));
+  }
+
+  constructor(props) {
+    super(props);
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.st = {showMenu: false};
+  }
+
   render() {
     const [IT, LogoutBtnI] = this.c2(T, LogoutBtn);
+    const active = this.st.showMenu ? bulma.isActive : '';
     return <nav class={bulma.navbar} role="navigation" aria-label="main navigation">
       <div class={bulma.navbarBrand}>
         <a class={bulma.navbarItem} href="/">
           <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
         </a>
 
-        <a role="button" class={bulma.navbarBurger}
+        <a role="button" class={bulma.navbarBurger} onClick={this.toggleMenu}
            aria-label="menu" aria-expanded="false" data-target="mainMenuAnchor">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -36,7 +49,7 @@ export class TitleMainMenu extends InjSubCom<TitleMainMenuP, TitleMainMenuS> {
         </a>
       </div>
 
-      <div id="mainMenuAnchor" class={bulma.navbarMenu + ' ' + bulma.isActive}>
+      <div id="mainMenuAnchor" class={bulma.navbarMenu + ' ' + active}>
         <div class={bulma.navbarStart}>
           <a class={bulma.navbarItem}>
             {this.props.t$title}
